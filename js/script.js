b69a090f1d134ce86ab400c1b94e24fd1d6e85d9
giocarvalho07 <giovannidecarvalhomendes@outlook.com>
2020-05-18 02:57:37 -0300

    portifolio 2020

---
+++ js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    M.AutoInit();
+  });
+
+document.addEventListener('DOMContentLoaded', function() {
+    M.FloatingActionButton();
+  });  
+
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+
+  });
+}
+const titulo = document.querySelector('.titulo-principal');
+typeWrite(titulo);
